91f0febc06bb3a41205b5c1ba1798d93f816752c
MayuraPathak <80159654+Path-may1@users.noreply.github.com>
2022-02-28 03:34:08 -0500

    added highscore functionality

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ console.log(document.querySelector('.guess').value)
 */
 
 
-
 //creating the secret number once at the start of the application and storing that in "number" called through document.querySelector method
 // Math.random method between 0 -20 - Math.random()*20
-const secretNumber = Math.trunc(Math.random()*20)+1;
-let score = 20; // score variable initiall sets the value to 20 at start of application and decreases the value by one each uses enters a number that is not equal to secret number
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20; // score variable initiall sets the value to 20 at start of //application and decreases the value by one each uses enters a number that is //not equal to secret number
 
-
+//highscore
+let highscore = 0;
 
 //Selecting the class "check" (of button in html) an adding addEventListerner method with function created as "click"- which displays what value selected by user in check box.
 
@@ -36,19 +35,72 @@ document.querySelector('.check').addEventListener('click', function() {
 
     //checking the validation of user input
 
+    //when there is no input
     if(!guess) {
         document.querySelector('.message').textContent ="⛔ No Number!";
+   //when there is input
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "✔ ✌ Correct Number";
+        // //displaying the correct number once the user wins the game
+        // document.querySelector('.number').textContent = secretNumber;
+        // //changing the background color once the user wins
+        // document.querySelector('body').style.backgroundColor ='#06b347'
+        // document.querySelection('.number').style.width='30rem'; 
+
+        // document.querySelector('.message').textContent = '🎉 Correct Number!';
+   // displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+
+      if (score > highscore) {
+      highscore = score;
+      console.log("highscore", highscore)
+      document.querySelector('.highscore').textContent = highscore;
+    }
     } else if(guess > secretNumber) {
-        document.querySelector('.message').textContent = 'Too high! Try again';
+
+        if(score > 1){ 
+              document.querySelector('.message').textContent = 'Too high! Try again';
         score = score - 1;
         document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = ' 💥You lost the game! Play Again'; 
+             document.querySelector('.score').textContent = 0;
+        }
+      
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'Too Low! Try again';
-        score--;
+        if(score > 1){ 
+              document.querySelector('.message').textContent = 'Too Low! Try again';
+        score = score - 1;
         document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = ' 💥You lost the game! Play Again'; 
+             document.querySelector('.score').textContent = 0;
+        }
     }
 
  
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+
+    document.querySelector('.message').textContent = "Start guessing...";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent= '?';
+    document.querySelector('.guess').value ='';
+    document.querySelector('body').style.backgroundColor='rgb(86, 117, 219)';
+    document.querySelector('.number').style.width='15rem';
+
+})
+
